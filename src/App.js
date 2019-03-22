32a6da88480c82fc36897a14acc7328786a8627b
madani6 <anandamadani06@gmail.com>
2019-03-22 16:37:52 +0700

    mulai dari 0 lagi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './component/Header/Header';
-import Jumbo from './component/Jumbo/Jumbo';
-import Cerdas from './component/Cerdas/Cerdas';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <div class="container">
-        <Jumbo />
-        </div>
-        <Cerdas />
+        
       </div>
-    );
+    )
   }
 }
-
-export default App;
